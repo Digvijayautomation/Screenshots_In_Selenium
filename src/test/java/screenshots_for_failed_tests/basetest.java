bed8df7bf96e4d96c2cd57f1f9151f6939dfe712
Digvijay <dtikka6@gmail.com>
2022-09-20 17:17:42 +0530

    screenshots

--- src/test/java/screenshots_for_failed_tests/basetest.java
+++ src/test/java/screenshots_for_failed_tests/basetest.java
@@ -11,9 +11,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
